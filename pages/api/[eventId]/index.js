da887ce1c77c84d1581c5719460efaf26c0df12e
Daniel Mendez <awilddanoru@gmail.com>
2023-05-11 14:27:46 -0700

    Remove unused code for posting new events

--- pages/api/[eventId]/index.js
+++ pages/api/[eventId]/index.js
@@ -1,8 +1,6 @@
 import {
   connectDatabase,
-  getAllDocuments,
   getFilteredDocuments,
-  insertDocument,
 } from "../../../src/helpers/db-util";
 
 async function handler(req, res) {
@@ -14,23 +12,6 @@ async function handler(req, res) {
     client = await connectDatabase();
   } catch (error) {
     res.status(500).json({ message: "Connecting to the database failed." });
-    return;
-  }
-
-  if (req.method === "POST") {
-    try {
-      if (isGuestOnly === true) {
-        await insertDocument(client, "events", { isGuestOnly: false });
-        client.close();
-      } else if (isGuestOnly === false) {
-        await insertDocument(client, "events", { isGuestOnly: true });
-        client.close();
-      }
-    } catch (error) {
-      res.status(500).json({ message: "Updating data failed." });
-      return;
-    }
-    res.status(201).json({ message: "Updated item successfully." });
     return;
   }
 
