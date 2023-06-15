dae61ba70094aabe2c71cece6d198ff7075507d0
Daniel Mendez <awilddanoru@gmail.com>
2023-06-15 12:10:45 -0700

    Update admin page to use MUI components and state

--- src/components/input/admin-item.jsx
+++ src/components/input/admin-item.jsx
@@ -1,5 +1,12 @@
-import { useRef } from "react";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import OutlinedInput from "@mui/material/OutlinedInput";
 import { useRouter } from "next/router";
+import { useRef, useState } from "react";
+import dynamic from "next/dynamic";
 
 function AdminItem(props) {
   const { eventId } = props;
@@ -8,11 +15,24 @@ function AdminItem(props) {
   const itemInputRef = useRef();
   const itemTypeInputRef = useRef();
 
-  function submitFormHandler() {
-    // event.preventDefault();
+  const [itemType, setItemType] = useState("");
+  const [item, setItem] = useState("");
 
-    const enteredItem = itemInputRef.current.value;
-    const enteredItemType = itemTypeInputRef.current.value;
+  const dynamicItemType = itemType === "guest-name" ? "Guest" : "Item";
+
+  const selectionHandler = (e) => {
+    setItemType(e.target.value);
+  };
+
+  const itemHandler = (e) => {
+    setItem(e.target.value);
+  };
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+
+    const enteredItem = item;
+    const enteredItemType = itemType;
     const enteredEventId = router.query.eventId;
 
     const reqBody = {
@@ -30,42 +50,73 @@ function AdminItem(props) {
     })
       .then((response) => response.json())
       .then((data) => console.log(data));
-  }
+  };
 
   return (
     <div>
       <form onSubmit={submitFormHandler}>
-        <label htmlFor="item">Add Item to Event List</label>
-        <select htmlFor="item-type" id="item-type" ref={itemTypeInputRef}>
-          <option value="recommendation" id="recommendation">
-            Recommendation
-          </option>
-          <option value="guest-name" id="guest-name">
-            Guest Name
-          </option>
-          <option value="main-dish" id="main-dish">
-            Main Dish
-          </option>
-          <option value="side-dish" id="side-dish">
-            Side Dish
-          </option>
-          <option value="snack" id="snack">
-            Snack
-          </option>
-          <option value="dessert" id="dessert">
-            Dessert
-          </option>
-          <option value="drink" id="drink">
-            Drink
-          </option>
-        </select>
-        <input
-          type="text"
-          id="item"
-          placeholder="Add Item"
-          ref={itemInputRef}
-        />
-        <button>Submit</button>
+        <FormControl
+          sx={{
+            m: 1,
+            width: "25ch",
+          }}
+          variant="outlined"
+        >
+          <InputLabel id="select-label">Type</InputLabel>
+          <Select
+            htmlFor="item-type"
+            id="item-type"
+            label="Type"
+            onChange={selectionHandler}
+            value={itemType}
+          >
+            <MenuItem value="recommendation" id="recommendation">
+              Recommendation
+            </MenuItem>
+            <MenuItem value="guest-name" id="guest-name">
+              Guest Name
+            </MenuItem>
+            <MenuItem value="main-dish" id="main-dish">
+              Main Dish
+            </MenuItem>
+            <MenuItem value="side-dish" id="side-dish">
+              Side Dish
+            </MenuItem>
+            <MenuItem value="snack" id="snack">
+              Snack
+            </MenuItem>
+            <MenuItem value="dessert" id="dessert">
+              Dessert
+            </MenuItem>
+            <MenuItem value="drink" id="drink">
+              Drink
+            </MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl
+          sx={{
+            m: 1,
+            width: "25ch",
+          }}
+        >
+          <InputLabel id="input-label">{dynamicItemType}</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment"
+            label={dynamicItemType}
+            onChange={itemHandler}
+          />
+        </FormControl>
+        <Button
+          sx={{
+            m: 1,
+            width: "25ch",
+            height: "7ch",
+          }}
+          variant="contained"
+          type="submit"
+        >
+          Add {dynamicItemType}
+        </Button>
       </form>
     </div>
   );
