529cce40a11c10b4969b83a2d479085ae4197fa2
Daniel Mendez <awilddanoru@gmail.com>
2023-08-01 12:43:30 -0700

    Transition eventId index from JavaScript to TypeScript

--- pages/events/[eventId]/index.tsx
+++ pages/events/[eventId]/index.tsx
@@ -8,11 +8,17 @@ import EventRSVPList from "../../../src/components/lists/event-rsvp-list";
 import HostRecommendations from "../../../src/components/admin/host-recommendations";
 import NewItem from "../../../src/components/input/new-item";
 
-function EventDetailPage(props) {
-  const [event] = props.data;
-  const [items, setItems] = useState([]);
+import { EVENT_DATA_TYPE, ITEM_ENTRY_TYPE } from "../../../src/types/index";
 
-  function updateItems(newItem) {
+interface Props {
+  data: EVENT_DATA_TYPE[];
+}
+
+function EventDetailPage(props: Props) {
+  const [event] = props.data;
+  const [items, setItems]: any = useState([]);
+
+  function updateItems(newItem: ITEM_ENTRY_TYPE) {
     const newItemEntry = { _id: "", itemEntry: newItem };
     setItems([...items, newItemEntry]);
   }
@@ -21,9 +27,7 @@ function EventDetailPage(props) {
 
   const humanReadableDate = moment(event.date).format("dddd, MMMM Do YYYY");
 
-  const timeUntilDate = moment(event.date)
-    .startOf("MMMM Do YYYY, h:mm:ss a")
-    .fromNow();
+  const timeUntilDate = moment(event.date).startOf("hour").fromNow();
 
   const checkTense = () => {
     if (moment(event.date).isSameOrAfter()) {
@@ -69,7 +73,7 @@ function EventDetailPage(props) {
   );
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({ params }: any) {
   const eventId = params.eventId;
 
   let dev = process.env.NODE_ENV !== "production";
@@ -86,7 +90,7 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export async function getStaticPaths(props) {
+export async function getStaticPaths(props: any) {
   const eventId = props.eventId;
 
   let dev = process.env.NODE_ENV !== "production";
@@ -95,7 +99,7 @@ export async function getStaticPaths(props) {
   let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/${eventId}`);
   let data = await response.json();
 
-  const paths = data.map((event) => ({ params: { eventId: event.id } }));
+  const paths = data.map((event: any) => ({ params: { eventId: event.id } }));
 
   return {
     paths: paths,
