ed3d3e882d7177ebe388d4e800767d49634947f5
Daniel Mendez <awilddanoru@gmail.com>
2023-08-15 13:40:51 -0700

    Add form to allow for editing event details

--- src/components/admin/edit-event.jsx
+++ src/components/admin/edit-event.jsx
@@ -1,7 +1,9 @@
 import Button from "@mui/material/Button";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
 import OutlinedInput from "@mui/material/OutlinedInput";
+import Select from "@mui/material/Select";
 
 import { DateTimePicker } from "@mui/x-date-pickers";
 import { useRouter } from "next/router";
@@ -21,8 +23,6 @@ function EditEvent() {
   const [location, setLocation] = useState("");
   const [date, setDate] = useState("");
   const [flyer, setFlyer] = useState("");
-  // const [isFeatured, setIsFeatured] = useState(false);
-  // const [isGuestOnly, setIsGuestOnly] = useState(false);
   const [eventData, setEventData] = useState([]);
 
   const router = useRouter();
@@ -38,49 +38,6 @@ function EditEvent() {
       });
   }, []);
 
-  // const toggleFeaturedEvent = () => {
-  //   fetch("/api/" + eventId + "/toggleValues", {
-  //     method: "PUT",
-  //     body: JSON.stringify({
-  //       id: eventId,
-  //       isFeatured: isChecked,
-  //     }),
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => console.log(data));
-  // };
-
-  // const idHandler = (e) => {
-  //   setId(e.target.value);
-  // };
-  // const titleHandler = (e) => {
-  //   setTitle(e.target.value);
-  // };
-
-  // const descriptionHandler = (e) => {
-  //   setDescription(e.target.value);
-  // };
-  // const locationHandler = (e) => {
-  //   setLocation(e.target.value);
-  // };
-
-  // const flyerHandler = (e) => {
-  //   setFlyer(e.target.value);
-  // };
-
-  // const featuredHandler = (e) => {
-  //   const toggleValue = e.target.checked;
-  //   setIsFeatured(toggleValue);
-  // };
-
-  // const guestOnlyHandler = (e) => {
-  //   const toggleValue = e.target.checked;
-  //   setIsGuestOnly(toggleValue);
-  // };
-
   const submitFormHandler = (e) => {
     e.preventDefault();
 
@@ -94,8 +51,6 @@ function EditEvent() {
       date: date,
       image: flyer,
       flyer: flyer,
-      isFeatured: isFeatured,
-      isGuestOnly: isGuestOnly,
     };
 
     fetch("/api/" + id, {
@@ -217,39 +172,53 @@ function EditEvent() {
             disabled
           />
         </FormControl>
-        {/* <FormControl
-        sx={{
-          m: 1,
-          width: "25ch",
-        }}
-      >
-        <FormControlLabel
-          label="Featured Event"
-          labelPlacement="start"
-          notched
-                    disabled
-        />
-      </FormControl>
-      <FormControl
-        sx={{
-          m: 1,
-          width: "25ch",
-        }}
-      >
-        <FormControlLabel
-          label="Guest Only"
-          labelPlacement="start"
-          notched
-                    disabled
-        />
-      </FormControl> */}
       </div>
-      <form>
-        <br />
+      <br />
+      <h3>Edit Event Details</h3>
+      <form onSubmit={submitFormHandler}>
+        <FormControl
+          sx={{
+            m: 1,
+            width: "25ch",
+          }}
+          variant="outlined"
+        >
+          <InputLabel id="select-label">Key</InputLabel>
+          <Select htmlFor="item-type" id="item-type" label="Type">
+            <MenuItem value="title" id="title">
+              Title
+            </MenuItem>
+            <MenuItem value="description" id="description">
+              Description
+            </MenuItem>
+            <MenuItem value="location" id="location">
+              Location
+            </MenuItem>
+            <MenuItem value="date" id="date">
+              Date
+            </MenuItem>
+            <MenuItem value="flyer" id="flyer">
+              Flyer
+            </MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl
+          sx={{
+            m: 1,
+            width: "25ch",
+          }}
+        >
+          <InputLabel id="input-label">Value</InputLabel>
+          <OutlinedInput id="outlined-adornment" label="value" />
+        </FormControl>
         <Button
+          sx={{
+            m: 1,
+            width: "25ch",
+            height: "7ch",
+          }}
           variant="contained"
           type="submit"
-          sx={{ margin: "10px" }}
           disabled
         >
           Submit Changes
