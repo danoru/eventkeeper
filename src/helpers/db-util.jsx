5be9eb9c82a8dc47cd9a0305de586fa6fd11cce9
Daniel Mendez <awilddanoru@gmail.com>
2023-08-25 13:57:41 -0700

    Fix error in helper function when connecting to client

--- src/helpers/db-util.jsx
+++ src/helpers/db-util.jsx
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export async function connectDatabase() {
-  const client = new MongoClient.connect(process.env.MONGODB_URI);
+  const client = new MongoClient(process.env.MONGODB_URI);
 
   return client;
 }
